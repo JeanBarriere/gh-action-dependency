2432856d0ae49b7d6e853714fd1c31bb53e71944
Jean Barriere <jean.barriere@shell.com>
2022-08-16 13:11:57 +0200

    chore: init

---
+++ index.mjs
@@ -0,0 +1,5 @@
+import dep from './modules/action-build-release.json' assert { type: 'json' };
+
+
+console.log('using dependency version:', dep.version);
+console.log('hello value is:', dep.hello);
